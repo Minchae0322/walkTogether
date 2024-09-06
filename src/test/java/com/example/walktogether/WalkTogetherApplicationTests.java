568550d64fadcf0450998506d95b531a42fb1cee
Minchae0322 <jmcabc0322@gmail.com>
2024-09-06 17:11:42 +0900

    first commit

---
+++ src/test/java/com/example/walktogether/WalkTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.walktogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkTogetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
