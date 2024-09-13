373fccb7e107c078d7a7a7af5d8b37530b857ed6
Minchae0322 <jmcabc0322@gmail.com>
2024-09-13 19:04:50 +0900

    setting: mysql database 설정

--- src/main/java/com/example/walktogether/WalkTogetherApplication.java
+++ src/main/java/com/example/walktogether/WalkTogetherApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WalkTogetherApplication {
 
-
-
     public static void main(String[] args) {
         SpringApplication.run(WalkTogetherApplication.class, args);
     }
