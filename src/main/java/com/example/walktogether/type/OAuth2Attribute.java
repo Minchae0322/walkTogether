f1e485925b4327b77388a1fb2b88a8132b863749
Minchae0322 <jmcabc0322@gmail.com>
2024-09-23 14:09:05 +0900

    feat: oAuth2 naver 추가 (네이버는 attribute 가 attribute{response {}} 구조라서 Map 을 찾은 후 username 설정

--- src/main/java/com/example/walktogether/type/OAuth2Attribute.java
+++ src/main/java/com/example/walktogether/type/OAuth2Attribute.java
@@ -4,6 +4,7 @@ import com.example.walktogether.util.OAuth2ServiceInfo;
 import com.example.walktogether.util.OAuth2UserParams;
 
 import java.util.Arrays;
+import java.util.Map;
 import java.util.function.Function;
 
 public enum OAuth2Attribute {
@@ -12,6 +13,15 @@ public enum OAuth2Attribute {
             .username((String) oAuth2ServiceInfo.getAttribute().get(oAuth2ServiceInfo.getUserNameAttributeName()))
             .attributes(oAuth2ServiceInfo.getAttribute())
             .build()
+    ),
+    NAVER("naver", (oAuth2ServiceInfo) -> {
+        Map<String, Object> userAttribute = (Map<String, Object>) oAuth2ServiceInfo.getAttribute().get("response");
+        return OAuth2UserParams.builder()
+                .providerId("naver")
+                .username((String) userAttribute.get("id"))
+                .attributes(userAttribute)
+                .build();
+    }
     ),;
 
     private final String providerId;
@@ -28,7 +38,7 @@ public enum OAuth2Attribute {
 
     public static OAuth2UserParams createOAuth2UserParams(String providerId, OAuth2ServiceInfo oAuth2ServiceInfo) {
         return Arrays.stream(values())
-                .filter(oAuth2Attribute -> oAuth2Attribute.providerId.equals(providerId))
+                .filter(o2 -> o2.getProviderId().equals(providerId))
                 .findFirst()
                 .orElseThrow(IllegalArgumentException::new)
                 .profileBuilder.apply(oAuth2ServiceInfo);
